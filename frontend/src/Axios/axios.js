4d7ea4d4b623fbb161074c4686ba997dc611c6a2
rohit <rkgupta9534@gmail.com>
2023-10-01 03:30:15 +0530

    backend

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+    baseURL:"https://todolistbackend-ncr9.onrender.com/api"
 })
 export default instance
